7ccf5dc5aed8d7c9bfc7014dddbee134ae2da366
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2024-02-19 20:54:36 -0300

    Finalização do componente Card News

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -20,6 +20,7 @@ class CardNews extends HTMLElement {
 
         const linkTitle = document.createElement('a');
         linkTitle.textContent = this.getAttribute('title');
+        linkTitle.href = this.getAttribute('link-url');
 
         const newsContent = document.createElement('p');
         newsContent.textContent = this.getAttribute('content');
@@ -32,6 +33,7 @@ class CardNews extends HTMLElement {
         cardRight.setAttribute('class', 'card__right');
 
         const newsImage = document.createElement('img');
+        newsImage.src = this.getAttribute('photo')  || 'assets/foto-default.jpg';
         cardRight.appendChild(newsImage);
         
         componentRoot.appendChild(cardLeft);
@@ -40,7 +42,45 @@ class CardNews extends HTMLElement {
         return componentRoot;
     }
 
-    styles() {}
+    styles() {
+        const style = document.createElement('style');
+        style.textContent = `
+            .card {
+                width: 80%;
+                box-shadow: 8px 8px 27px -6px rgba(0,0,0,0.75);
+                -webkit-box-shadow: 8px 8px 27px -6px rgba(0,0,0,0.75);
+                -moz-box-shadow: 8px 8px 27px -6px rgba(0,0,0,0.75);
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+            }
+            
+            .card__left {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+            
+            .card__left > span {
+                font-weight: 400;
+            }
+            
+            .card__left > a {
+                margin-top: 15px;
+                font-size: 25px;
+                color: black;
+                text-decoration: none;
+                font-weight: bold;
+            }
+            
+            .card__left > p {
+                color: rgb(70, 70, 70);
+            }
+        `;
+
+        return style;
+    }
 
 }
 
